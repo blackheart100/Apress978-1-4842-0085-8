1cfaf4829c20275f354df6dd805dfe43870b5a69
blackheart100 <1290823140@qq.com>
2018-01-06 14:08:21 +0800

    84

---
+++ Apress978-1-4842-0085-8/SportsStore/App_Start/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportsStore.IdentityConfig))]
+namespace SportsStore
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
